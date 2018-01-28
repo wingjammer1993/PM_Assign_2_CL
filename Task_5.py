8102378024ecae16422425ad9d32bdd6a58e0cf4
Amruta <hail.radical@gmail.com>
2018-01-27 19:03:57 -0700

    Assignment 2

--- Task_5.py
+++ Task_5.py
@@ -13,19 +13,19 @@ if __name__ == "__main__":
     # part a
     posterior_3 = Task_2.give_posterior_probability(sigma_30, [(2.2, -0.2)])
     print(posterior_3)
-    gen_pred_2 = Task_3.give_general_prediction(y_values, h_values, posterior_3)
-    print(gen_pred_2)
-    Task_3.plot_contour(y_values, gen_pred_2, "Generalized Predictions with X = {(2.2, -2)}")
+    gen_pred_3 = Task_3.give_general_prediction(y_values, h_values, posterior_3)
+    print(gen_pred_3)
+    Task_3.plot_contour(y_values, gen_pred_3, "Generalized Predictions with X = {(2.2, -2)}")
     # part b
     posterior_4 = Task_2.give_posterior_probability(sigma_30, [(2.2, -0.2), (0.5, 0.5)])
     print(posterior_4)
-    gen_pred_3 = Task_3.give_general_prediction(y_values, h_values, posterior_4)
-    print(gen_pred_3)
-    Task_3.plot_contour(y_values, gen_pred_3, "Generalized Predictions with X = {(2.2, -2),(0.5, 0.5)}")
+    gen_pred_4 = Task_3.give_general_prediction(y_values, h_values, posterior_4)
+    print(gen_pred_4)
+    Task_3.plot_contour(y_values, gen_pred_4, "Generalized Predictions with X = {(2.2, -2),(0.5, 0.5)}")
     # part c
     posterior_5 = Task_2.give_posterior_probability(sigma_30, [(2.2, -0.2), (0.5, 0.5), (1.5, 1)])
     print(posterior_5)
-    gen_pred_4 = Task_3.give_general_prediction(y_values, h_values, posterior_5)
-    print(gen_pred_4)
-    Task_3.plot_contour(y_values, gen_pred_4, "Generalized Predictions with X = {(2.2, -2),(0.5, 0.5), (1.5, 1)}")
+    gen_pred_5 = Task_3.give_general_prediction(y_values, h_values, posterior_5)
+    print(gen_pred_5)
+    Task_3.plot_contour(y_values, gen_pred_5, "Generalized Predictions with X = {(2.2, -2),(0.5, 0.5), (1.5, 1)}")
 
